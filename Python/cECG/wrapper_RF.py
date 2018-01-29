e4e907bffe06d5beaf8ffd43b831041f0b2b175b
JanderHungrige <werth.jan@gmail.com>
2018-01-29 10:56:48 +0100

    Hier das ist fuer ECG 1234
    Erreicht nicht ganz die werte im Paper?
    Kleine spritze noetig
    
    Ist nicht wirklich optimiert

--- Python/cECG/wrapper_RF.py
+++ Python/cECG/wrapper_RF.py
@@ -68,7 +68,7 @@ Loading data declaration & Wrapper variables
 **************************************************************************
 """
 Rpeakmethod='R' #R or M
-dataset='cECG'  # Either ECG or cECG and later maybe MMC or InnerSense
+dataset='ECG'  # Either ECG or cECG and later maybe MMC or InnerSense
 #***************
 #selectedbabies =[0,2,3,5,7] #0-8 ('4','5','6','7','9','10','11','12','13')
 selectedbabies=[0,1,2,3,5,6,7,8]
@@ -76,6 +76,7 @@ label=[1,2,3,4] # 1=AS 2=QS 3=Wake 4=Care-taking 5=NA 6= transition
 #---------------------------
 # Feature list
 lst = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,32] 
+
 #lst_old=[3,4,5,6,7,8,9,10,11,14,15,16,17,18,19,20,21,22,23,24,25,26] # From first paper to compare with new features
 #lst=lst_old
 #---------------------------
@@ -85,9 +86,9 @@ scaling='Z' # Scaling Z or MM
 Movingwindow=10 # WIndow size for moving average
 preaveraging=0
 postaveraging=1
-exceptNOF=1 #Which Number of Features (NOF) should be used with moving average?  all =oth tzero; only some or all except some defined in FEAT
+exceptNOF=0 #Which Number of Features (NOF) should be used with moving average?  all =oth tzero; only some or all except some defined in FEAT
 onlyNOF=0 # [0,1,2,27,28,29]
-FEAT=[0,1,2]# 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26
+FEAT=[0,1,2]# cECG 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26
 #----------------------------
 PolyTrans=0#use polinominal transformation on the Features specified in FEATp
 ExpFactor=2# which degree of polinomonal (2)
@@ -199,16 +200,19 @@ if 4 in label:
        #---------------------------
        Movingwindow=47 # WIndow size for moving average
        preaveraging=0
-       postaveraging=1
+       postaveraging=0
        exceptNOF=1 #Which Number of Features (NOF) should be used with moving average?  all =oth tzero; only some or all except some defined in FEAT
        onlyNOF=0 # [0,1,2,27,28,29]
        FEAT=[0,1,2,29]
+       FEAT=[0,1,2,27,28,29]
+
        #----------------------------
        PolyTrans=1#use polinominal transformation on the Features specified in FEATp
        ExpFactor=2# which degree of polinomonal (2)
        exceptNOpF= 0#Which Number of Features (NOpF) should be used with polynominal fit?  all =0; only some or all except some defined in FEATp
        onlyNOpF=1 # [0,1,2,27,28,29]
-       FEATp=[0,3,4,5,30]
+       FEATp=[0,3,4,5]
+       
        #---------------------------
        SamplingMeth='NONE'  # 'NONE' 'SMOTE'  or 'ADASYN' #For up and downsampling of data
        ChoosenKind=1   # 0-3['regular','borderline1','borderline2','svm'] only when using SMOTE
@@ -217,7 +221,7 @@ if 4 in label:
        ASprobLimit=[0.70,0.7]# Determine the AS lower limit for the probability for which another class is chosen than AS. For: [3 labels, >3 labels]
        #--------------------
        Used_classifier='RF' #RF=random forest ; ERF= extreme random forest; TR= Decission tree; GB= Gradient boosting
-       N=50 # Estimators for the trees
+       N=500 # Estimators for the trees
        crit='entropy' #gini or entropy method for trees 
        msl=3  #min_sample_leafe
        deciding_performance_measure='Kappa' #Kappa , F1_second_label, F1_third_label, F1_fourth_label 
