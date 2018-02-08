fe4fbba29c74041a5695f3e5c76486cf991a36a4
JanderHungrige <werth.jan@gmail.com>
2018-02-08 12:58:19 +0100

    34/15/28
    QS and IS -30/31 (more AS)
    KONFMAT 619 172 151
    
    also good if only IS -30/31 (more QS)
    571 187 151

--- Python/cECG/wrapper_RF.py
+++ Python/cECG/wrapper_RF.py
@@ -70,8 +70,8 @@ Loading data declaration & Wrapper variables
 lstQS=[0,1,2,3,4,5,7,8,9,11,12,13,14,15,16,17,19,20,21,22,24,26,27,28,29,31,32,33] 
 lstIS=[0,1,2,5,6,7,8,9,11,12,13,14,15,16,17,18,20,21,22,23,24,25,26,27,28,29,32,33] 
 
-lstQS=[0,1,2,3,4,6,7,10,11,12,13,14,16,17,18,20,21,22,23,24,25,26,27,28,29,30,31,32,33] 
-lstIS=[1,2,3,6,7,10,11,12,13,14,15,16,17,18,19,21,22,23,24,25,26,27,28,29,30,31,32,33]  
+lstQS=[0,1,2,3,4,6,7,10,11,12,13,14,16,17,18,20,21,22,23,24,25,26,27,28,29,32,33] 
+lstIS=[1,2,3,6,7,10,11,12,13,14,15,16,17,18,19,21,22,23,24,25,26,27,28,29,32,33]  
 ASQS=[0.65,0.7]
 ASIS=[0.73,0.7]
 
@@ -361,7 +361,8 @@ RES1_F1_all_mean=array(mean(RES1_F1_all,0))
 RES1_KAPPA_overall=cohen_kappa_score(tmp_orig.ravel(),tmp_pred.ravel(),labels=label)
 KonfMATall.append(confusion_matrix(tmp_orig.ravel(), tmp_pred.ravel(), labels=label, sample_weight=None))
 
-
+FimportanceMean_QS=mean(Fimportance_QS,0)
+FimportanceMean_IS=mean(Fimportance_IS,0)
 """
 END
 """
