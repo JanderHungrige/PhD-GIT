c1991b9a41b3b1af7ceaa802b31574e67165182e
JanderHungrige <werth.jan@gmail.com>
2018-02-08 14:59:13 +0100

    36/12/33
    pol IS 1,32
    681 174 151
    
    pol IS 1,2,3
    687 156 161
    
    pol IS 1
    673 176 150
    
    only IS pol 32 lower overall kappa but 160 160 on konfamt for QS, IS

--- Python/cECG/wrapper_RF.py
+++ Python/cECG/wrapper_RF.py
@@ -67,16 +67,16 @@ Loading data declaration & Wrapper variables
 27-33 nonlin
 **************************************************************************
 """
-lstQS=[0,1,2,3,4,5,7,8,9,11,12,13,14,15,16,17,19,20,21,22,24,26,27,28,29,31,32,33] 
-lstIS=[0,1,2,5,6,7,8,9,11,12,13,14,15,16,17,18,20,21,22,23,24,25,26,27,28,29,32,33] 
+
 
 lstQS=[0,1,2,3,4,6,7,10,11,12,13,14,16,17,18,20,21,22,23,24,25,26,27,28,29,32,33] 
 lstIS=[1,2,3,6,7,10,11,12,13,14,15,16,17,18,19,21,22,23,24,25,26,27,28,29,32,33]  
 ASQS=[0.65,0.7]
 ASIS=[0.73,0.7]
 
-PolTQS=0; FEATpQS=[]#[0,3,4,5]
-PolTIS=0; FEATpIS=[]#lstIS.index(8),lstIS.index(9),lstIS.index(10),lstIS.index(15),lstIS.index(16),lstIS.index(23)]#456 11 12 27
+PolTQS=0; FEATpQS=[]
+PolTIS=1; FEATpIS=[lstIS.index(1),lstIS.index(32)]
+
 FEATaQS=[lstQS.index(1),lstQS.index(4),lstQS.index(13),lstQS.index(14),lstQS.index(16),lstQS.index(18),lstQS.index(21),lstQS.index(23),lstQS.index(24),lstQS.index(25)] #lstQS.index(15)
 FEATaIS=[lstIS.index(2),lstIS.index(13),lstIS.index(14),lstIS.index(16),lstIS.index(18),lstIS.index(21),lstIS.index(23),lstIS.index(24),lstIS.index(25),lstIS.index(26),lstIS.index(33)]#lstIS.index(20)
 
